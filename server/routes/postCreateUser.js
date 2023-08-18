1a495b8c63e9f67d61d715244cab19d555fe4c33
frazerlinscott <frazerlinscott@me.com>
2023-08-18 16:52:42 +1000

    Final Commit
    Testing

--- server/routes/postCreateUser.js
+++ server/routes/postCreateUser.js
@@ -21,11 +21,13 @@ module.exports = function(req, res) {
         // Check if the user already exists
         const existingUser = users.find(u => u.username === newUser.username || u.email === newUser.email);
         if (existingUser) {
+            console.log("Already existing user");
             return res.status(400).json({ success: false, message: 'Username or Email already exists.' });
         }
 
         // Push the new user to the list
         users.push(newUser);
+        console.log("User added successfully");
 
         // Write back to the file
         fs.writeFile(path.join(__dirname, '..', 'data', 'usersData.json'), JSON.stringify(users, null, 2), 'utf8', (err) => {
